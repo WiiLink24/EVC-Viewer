047c1e6727bf1e0e989063dcc67aee6a80a95ea3
kouacc <maxrur57@gmail.com>
2024-07-30 16:42:29 +0200

    feat: basic id page

--- frontend/src/backend.ts
+++ frontend/src/backend.ts
@@ -3,12 +3,12 @@ export const api = import.meta.env.VITE_API_URL
 
 
 export async function fetchCurrentPolls(page: number, language: string, limit?: number, type?: 'all' | 'n' | 'w') {
-  const response = await axios.get(`${api}/api/polls?page=${page} ${limit ? `&limit=${limit}` : ''} ${type ? `&type=${type}` : ''} &language=${language}`)
+  const response = await axios.get(`${api}/api/polls?page=${page}${limit ? `&limit=${limit}` : ''}${type ? `&type=${type}` : ''}&language=${language}`)
   return response.data
 }
 
 export async function fetchPoll(id: number, language: string, country?: number, region?: number, details?: boolean) {
-  const response = await axios.get(`${api}/api/polls/poll/${id}?language=${language} ${country ? `&country=${country}` : ''} ${region ? `&region=${region}` : ''} ${details ? `&details=${details}` : ''}`)	
+  const response = await axios.get(`${api}/api/polls/poll/${id}?language=${language} ${country ? `&country=${country}` : ''}${region ? `&region=${region}` : ''} ${details ? `&details=${details}` : ''}`)	
   return response.data
 }
 
--- frontend/src/pages/polls/[id].vue
+++ frontend/src/pages/polls/[id].vue
@@ -4,16 +4,28 @@ import { useRoute } from 'vue-router'
 import { fetchPoll } from '@/backend'
 
 const route = useRoute()
-const id = ref(route.params.id) 
+const id = ref(route.params.id)
+
+const isLoading = ref(false)
 
 const poll = ref()
-const isLoading = ref(false)
+const poll_data = ref()
+const votes_data = ref()
+const details_data = ref()
+
+const language = ref(localStorage.getItem('language') || 'english')
+const country = ref(parseInt(localStorage.getItem('country')) || 49)
 
 onMounted(async () => {
     console.log(id.value)
     try {
         isLoading.value = true
-        poll.value = await fetchPoll(id.value, language)
+        console.log(language.value)
+        poll.value = await fetchPoll(id.value, language.value)
+        poll_data.value = poll.value.poll_data
+        votes_data.value = poll.value.results_data
+        console.log(poll.value)
+        console.log(poll_data.value)
     } catch (error) {
         console.error(error)
     } finally {
@@ -23,5 +35,14 @@ onMounted(async () => {
 </script>
 
 <template>
-    <h1>{{ id }}</h1>
+    <div v-if="poll">
+        <h1>{{ poll_data.content }}</h1>
+        <span>{{ poll_data.choice1 }}</span><span>{{ poll_data.choice2 }}</span>
+        <span v-if="poll_data.type = 'n'">National poll</span>
+        <span v-else-if="poll_data.type = 'w'">Worldwide poll</span>
+
+        <!-- use votes_data to fetch results-->
+        <!-- <span>{{ votes_data.total_votes }}</span> -->
+    </div>
+
 </template>
